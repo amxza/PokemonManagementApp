358467970bb3cddeda812856b6bde77ca6a80965
Amaan Mirza <amaanm3790@gmail.com>
2026-01-27 08:23:12 -0600

    Displays the Trainer's Pokemon thru searching for ID

--- controllers/controls.js
+++ controllers/controls.js
@@ -2,20 +2,29 @@
 const trainers = [
     {
         trainerName: "Ash",
-        id: Date.now().toString(),
-        pokemon: ["Pikachu", "Charizard", "Squirtle"]
+        id: 1,
+        pokemon: [
+            {name: "Pikachu", type: "Electric", description: "A yellow Mouse Pokemon"},
+            {name: "Charizard", type: "Fire", description: "A Fire Dragon Pokemon"},
+            {name: "Squirtle", type: "Water", description: "A water turtle pokemon"},
+        ],
     },
     {
         trainerName: "Misty",
-        id: Date.now().toString(),
-        pokemon: ["Togepi", "Psyduck"]
+        id: 2,
+        pokemon: [
+            {name: "Togepi", type: "Normal", description: "The Eggshell Pokemon"},
+            {name: "Psyduck", type: "Water", description: "THe Duck Pokemon"},
+        ],
     },
     {
         trainerName: "Brock",
-        id: Date.now().toString(),
-        pokemon: ["Onix"]
-    }
-]
+        id: 3,
+        pokemon: [
+            {name: "Onix", type: "Rock", description: "The Rock Snake Pokemon"},
+        ],
+    },
+];
 
 async function getAll(req, res) {
     res.render("index", {title: "MainPage", trainers: trainers});
@@ -27,13 +36,20 @@ async function newTrainer(req, res) {
 
 async function getNewTrainer(req, res) {
     const newTrainer = req.body.newTrainer
-    trainers.push({trainerName: newTrainer, id: Date.now().toString(), pokemon: []});
+    trainers.push({trainerName: newTrainer, id: trainers.length + 1, pokemon: []});
     res.redirect("/");
 }
-
 async function viewTrainerPokemon(req, res) {
-    
-}
+    const trainerID = req.params.id;
+
+    const tPokemon = trainers.find(p => String(p.id) === trainerID);
+
+    if (!tPokemon) {
+        return res.status(404).send("Player Not Found...");
+    }
+
+    res.render("trainerPokemon", {tPokemon: tPokemon});
+};
 
 
 module.exports = {
@@ -41,4 +57,6 @@ module.exports = {
     getAll,
     newTrainer,
     getNewTrainer,
+    viewTrainerPokemon,
 }
+
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -5,5 +5,6 @@ const control = require("../controllers/controls");
 indexRoute.get("/", control.getAll);
 indexRoute.get("/new", control.newTrainer);
 indexRoute.post("/new", control.getNewTrainer);
+indexRoute.get("/trainer/:id", control.viewTrainerPokemon);
 
 module.exports = indexRoute;
