116e14f893682fb3e3a2dda941c6434250460c45
Amaan Mirza <amaanm3790@gmail.com>
2026-01-29 16:00:05 -0600

    Completed layout and routes using hardcoded inputs

--- controllers/controls.js
+++ controllers/controls.js
@@ -4,29 +4,29 @@ const trainers = [
         trainerName: "Ash",
         id: 1,
         pokemon: [
-            {name: "Pikachu", type: "Electric", description: "A yellow Mouse Pokemon"},
-            {name: "Charizard", type: "Fire", description: "A Fire Dragon Pokemon"},
-            {name: "Squirtle", type: "Water", description: "A water turtle pokemon"},
+            {id: 101, name: "Pikachu", type: "Electric", description: "A yellow Mouse Pokemon"},
+            { id: 102, name: "Charizard", type: "Fire", description: "A Fire Dragon Pokemon"},
+            {id: 103, name: "Squirtle", type: "Water", description: "A water turtle pokemon"},
         ],
     },
     {
         trainerName: "Misty",
         id: 2,
         pokemon: [
-            {name: "Togepi", type: "Normal", description: "The Eggshell Pokemon"},
-            {name: "Psyduck", type: "Water", description: "THe Duck Pokemon"},
+            {id: 104, name: "Togepi",  type: "Normal", description: "The Eggshell Pokemon"},
+            {id: 105, name: "Psyduck", type: "Water", description: "THe Duck Pokemon"},
         ],
     },
     {
         trainerName: "Brock",
         id: 3,
         pokemon: [
-            {name: "Onix", type: "Rock", description: "The Rock Snake Pokemon"},
+            {id: 106, name: "Onix", type: "Rock", description: "The Rock Snake Pokemon"},
         ],
     },
 ];
 
-async function getAll(req, res) {
+async function getAllTrainers(req, res) {
     res.render("index", {title: "MainPage", trainers: trainers});
 }
 
@@ -51,12 +51,57 @@ async function viewTrainerPokemon(req, res) {
     res.render("trainerPokemon", {tPokemon: tPokemon});
 };
 
+async function addPokemon(req, res) {
+    const newPoke = trainers.find(p => String(p.id) === req.params.id);
+
+    res.render("newPokemon", {newPoke: newPoke})
+}
+
+async function getAddPokemon(req, res) {
+    const trainerID = req.params.id;
+    const {name, type, description} = req.body;
+
+    const newPoke = trainers.find(p => String(p.id) === trainerID);
+
+    if(newPoke) {
+        newPoke.pokemon.push({
+            id: Date.now(),
+            name: name,
+            type: type,
+            description: description,
+        });
+        res.redirect(`/trainer/${trainerID}`);
+    } else {
+        res.status(404).send("Pokemon not found");
+    }
+};
+
+async function getPokemonDetails(req, res) {
+    const {id, pokeId} = req.params;
+
+    const trainer = trainers.find(t => String(t.id) === id);
+    if (!trainer) return res.status(404).send("Trainer Not Found");
+
+    const pokemon = trainer.pokemon.find(p => String(p.id) === pokeId);
+    if(!pokemon) return res.status(404).send("Pokemon not found");
+
+    res.render("pokemonDetails", {trainer, pokemon})
+}
+
+async function getAllPokemon(req, res) {
+    res.render("allPokemon", {trainers: trainers});
+}
+
 
 module.exports = {
     trainers,
-    getAll,
+    getAllTrainers,
     newTrainer,
     getNewTrainer,
     viewTrainerPokemon,
+    addPokemon,
+    getAddPokemon,
+    getPokemonDetails,
+    getAllPokemon,
 }
 
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -2,9 +2,12 @@ const {Router} = require("express");
 const indexRoute = Router();
 const control = require("../controllers/controls");
 
-indexRoute.get("/", control.getAll);
+indexRoute.get("/", control.getAllTrainers);
 indexRoute.get("/new", control.newTrainer);
 indexRoute.post("/new", control.getNewTrainer);
 indexRoute.get("/trainer/:id", control.viewTrainerPokemon);
-
+indexRoute.get("/trainer/:id/newpokemon", control.addPokemon);
+indexRoute.post("/trainer/:id/newpokemon", control.getAddPokemon);
+indexRoute.get("/trainer/:id/pokemon/:pokeId", control.getPokemonDetails);
+indexRoute.get("/allpokemon", control.getAllPokemon);
 module.exports = indexRoute;
